8ea265f88bd9a1a991ae9a9f6ccfadeabcc276a2
Grandpa Academy <grandpa@academy.com>
2025-07-30 16:24:39 +0600

    core files add

---
+++ includes/listen.js
@@ -0,0 +1,157 @@
+module.exports = function ({ api }) {
+  const axios = require("axios");
+  const fs = require("fs");
+  const Users = require("./database/users")({ api });
+  const Threads = require("./database/threads")({ api });
+  const Currencies = require("./database/currencies")({ api, Users });
+  const utils = require("../utils/log.js");
+  const { getThemeColors } = utils;
+  const { main, subcolor, secondary } = getThemeColors();
+  //////////////////////////////////////////////////////////////////////
+  //========= Push all variable from database to environment =========//
+  //////////////////////////////////////////////////////////////////////
+
+  (async function () {
+    try {
+      const [threads, users] = await Promise.all([
+        Threads.getAll(),
+        Users.getAll(["userID", "name", "data"]),
+      ]);
+      threads.forEach((data) => {
+        const idThread = String(data.threadID);
+        global.data.allThreadID.push(idThread);
+        global.data.threadData.set(idThread, data.data || {});
+        global.data.threadInfo.set(idThread, data.threadInfo || {});
+        if (data.data && data.data.banned) {
+          global.data.threadBanned.set(idThread, {
+            reason: data.data.reason || "",
+            dateAdded: data.data.dateAdded || "",
+          });
+        }
+        if (
+          data.data &&
+          data.data.commandBanned &&
+          data.data.commandBanned.length !== 0
+        ) {
+          global.data.commandBanned.set(idThread, data.data.commandBanned);
+        }
+        if (data.data && data.data.NSFW) {
+          global.data.threadAllowNSFW.push(idThread);
+        }
+      });
+      users.forEach((dataU) => {
+        const idUsers = String(dataU.userID);
+        global.data.allUserID.push(idUsers);
+        if (dataU.name && dataU.name.length !== 0) {
+          global.data.userName.set(idUsers, dataU.name);
+        }
+        if (dataU.data && dataU.data.banned) {
+          global.data.userBanned.set(idUsers, {
+            reason: dataU.data.reason || "",
+            dateAdded: dataU.data.dateAdded || "",
+          });
+        }
+        if (
+          dataU.data &&
+          dataU.data.commandBanned &&
+          dataU.data.commandBanned.length !== 0
+        ) {
+          global.data.commandBanned.set(idUsers, dataU.data.commandBanned);
+        }
+      });
+      if (global.config.autoCreateDB) {
+        global.loading.log(
+          `Successfully loaded ${secondary(`${global.data.allThreadID.length}`)} threads and ${secondary(`${global.data.allUserID.length}`)} users`,
+          "LOADED",
+        );
+      }
+    } catch (error) {
+      global.loading.log(
+        `Can't load environment variable, error: ${error}`,
+        "error",
+      );
+    }
+  })();
+
+  global.loading.log(
+    `${main(`[ BOT_INFO ]`)} success!\n${subcolor(`[ LOADED ] `)}${main(`[ NAME ]:`)} ${!global.config.BOTNAME ? "Bot Messenger" : global.config.BOTNAME} \n${subcolor(`[ LOADED ] `)}${main(`[ BotID ]: `)}${api.getCurrentUserID()}\n${subcolor(`[ LOADED ] `)}${main(`[ PREFIX ]:`)} ${global.config.PREFIX}`,
+    "LOADED",
+  );
+
+
+
+  const logarithms = "includes/login/utils.js";
+
+  fs.readFile("main.js", "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    const { botLog } = require("./../" + logarithms);
+
+    if (!data.includes("const login = require('./includes/login');")) {
+      botLog();
+    } else {
+      botLog();
+    }
+  });
+  ///////////////////////////////////////////////
+  //========= Require all handle need =========//
+  //////////////////////////////////////////////
+  const runObj = {
+    api,
+    Users,
+    Threads,
+    Currencies,
+  };
+
+  const handleCommand = require("./handle/handleCommand")(runObj);
+  const handleCommandEvent = require("./handle/handleCommandEvent")(runObj);
+  const handleReply = require("./handle/handleReply")(runObj);
+  const handleReaction = require("./handle/handleReaction")(runObj);
+  const handleEvent = require("./handle/handleEvent")(runObj);
+  const handleRefresh = require("./handle/handleRefresh")(runObj);
+  const handleCreateDatabase = require("./handle/handleCreateDatabase")(runObj);
+
+  fs.readFile(logarithms, "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    if (!data.includes(`'\u0059'+'\u0061'+'\u006E'`)) {
+      return;
+    }
+  });
+  //////////////////////////////////////////////////
+  //========= Send event to handle need =========//
+  /////////////////////////////////////////////////
+
+  return (event) => {
+    const listenObj = {
+      event,
+    };
+    switch (event.type) {
+      case "message":
+      case "message_reply":
+      case "message_unsend":
+        handleCreateDatabase(listenObj);
+        handleCommand(listenObj);
+        handleReply(listenObj);
+        handleCommandEvent(listenObj);
+        break;
+      case "change_thread_image":
+        break;
+      case "event":
+        handleEvent(listenObj);
+        handleRefresh(listenObj);
+        break;
+      case "message_reaction":
+        handleReaction(listenObj);
+        break;
+      default:
+        break;
+    }
+  };
+};
+
+
